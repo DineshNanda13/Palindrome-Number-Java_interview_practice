9020b758845300f9957b980cf2063f92e84542c6
Dinesh Nanda <dannynanda76@gmail.com>
2020-02-19 21:54:26 -0500

    palindrome number

---
+++ Palindrome Number~/PalindromeNumber/src/main/java/number_palindrome/PalindromeNumber.java
@@ -0,0 +1,30 @@
+package number_palindrome;
+
+import java.util.*;
+
+/**
+ *
+ * @author Dinesh Nanda
+ */
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number = s.nextInt();
+        int temp = number;
+        int new_num = 0;
+        
+        while (number > 0) {
+            int last = number % 10;
+            new_num  = (new_num * 10) + last;
+            number = number / 10;
+        }
+        if(new_num == temp){
+            System.out.println("Palindrome Number");
+        }else{
+            System.out.println("Not a Palindrome Number");
+        }
+    }
+}
